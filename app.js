c40327f25df12ad7a9915121a6ff8c25a4a5c187
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-08-20 21:07:26 +0300

    057 Class Methods and Access Modifiers

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 27;
         this.name = name;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType("Walker");
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person("Nick", 'nick');
 console.log(person);
+person.printAge();
+// person.setType("Cool guy");
--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
 class Person {
   name: string;
   private type: string;
-  protected age: number;
+  protected age: number = 27;
 
   constructor(name: string, public username: string) {
     this.name = name;
+  }
+
+  printAge() {
+    console.log(this.age);
+    this.setType("Walker")
+  }
+
+  private setType(type: string) {
+    this.type = type;
+    console.log(this.type);
   }
 }
 
 const person = new Person("Nick", 'nick');
 console.log(person);
+
+person.printAge();
+// person.setType("Cool guy");
